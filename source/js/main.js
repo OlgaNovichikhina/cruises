8922b26156358bb201a8ec1d286e0d5bc53997ef
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-11-08 22:25:09 +0300

    нарезала изображения, разметила и частично стилизовала шапку

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {clickOnToggle} from './modules/modals/burger';
 
 // ---------------------------------
 
@@ -9,6 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  clickOnToggle();
 
   // Modules
   // ---------------------------------
